a4ab20f6a460d2b1a005a98a5ea94b687a28ffc5
Thiha Htet Zaw <thihahtetzaw7991@gmail.com>
2025-02-13 17:39:26 +0700

    Create Subreddit View

--- convex/subreddit.ts
+++ convex/subreddit.ts
@@ -1,23 +1,36 @@
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { getCurrentUserOrThrow } from "./users";
-import { ConvexError, v } from "convex/values";
+import { ConvexError, convexToJson, v } from "convex/values";
 
 export const create = mutation({
-    args: {
-        name: v.string(),
-        description: v.optional(v.string()),
-    },
-    handler: async (ctx, args) => {
-        const user = await getCurrentUserOrThrow(ctx);
-        const subreddits = await ctx.db.query("subreddit").collect();
-        if(subreddits.some(subreddit => subreddit.name === args.name)) {
-            throw new ConvexError("Subreddit already exists");
-        }
-        await ctx.db.insert("subreddit", {
-            name: args.name,
-            description: args.description,
-            authorId: user._id,
-        });
+  args: {
+    name: v.string(),
+    description: v.optional(v.string()),
+  },
+  handler: async (ctx, args) => {
+    const user = await getCurrentUserOrThrow(ctx);
+    const subreddits = await ctx.db.query("subreddit").collect();
+    if (subreddits.some((subreddit) => subreddit.name === args.name)) {
+      throw new ConvexError("Subreddit already exists");
     }
+    await ctx.db.insert("subreddit", {
+      name: args.name,
+      description: args.description,
+      authorId: user._id,
+    });
+  },
+});
 
-})
+export const get = query({
+  args: { name: v.string() },
+  handler: async (ctx, args) => {
+    const subreddit = await ctx.db
+      .query("subreddit")
+      .filter((q) => q.eq(q.field("name"), args.name))
+      .unique();
+
+    if (!subreddit) return null;
+
+    return subreddit;
+  },
+});
